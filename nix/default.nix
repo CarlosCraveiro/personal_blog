99a4e3d7ae0332cc3225d8499c8d2eba0baeed45
Carlos Craveiro <CarlosCraveiro@usp.br>
2024-01-08 22:19:00 -0300

    fixes feed indexing

--- nix/default.nix
+++ nix/default.nix
@@ -9,7 +9,9 @@ let
     rev = "v2.49.1";
     sha256 = "sha256-SHBGMV+IUBLYsYklt8NHGpWxzRHmwbK//o6pwFmmlbE="; # Still have to discover the hash 
   };
-  repo_name = "personal_blog"; # Need for GitHub Pages correct css indexing (not /css and yes /repo_name/css)
+  repo_name = "personal_blog";
+  repo_url = "https://carloscraveiro.github.io/personal_blog"; # Need for GitHub Pages correct css indexing (not /css and yes /repo_name/css)
+
 in pkgs.stdenv.mkDerivation {
     name = "${repo_name}";
     src = ./../.;
@@ -17,7 +19,7 @@ in pkgs.stdenv.mkDerivation {
     buildPhase = "
         rm -df themes/blowfish
         ln ${blowfish} -sfT themes/blowfish
-        hugo --gc --minify --baseURL /${repo_name}
+        hugo --gc --minify --baseURL ${repo_url}/
         ";
     
     installPhase = "cp -r public $out";
